c297dfe9e5faf25df984d63fb95751589901774e
Edward Grekoski <eddiegrekoksi@gmail.com>
2016-02-16 19:01:57 -0500

    no more code!

---
+++ Linear_Actuator_Reader/Linear_Actuator_Reader.ino
@@ -0,0 +1,13 @@
+
+  delay(1000);
+  Serial.print(sensorValue);
+  Serial.print(outputValue);
+  delay(2);
+  }
+  digitalWrite (13, LOW);
+  digitalWrite (12, HIGH);
+  delay(10000);
+  
+  // put your main code here, to run repeatedly:
+
+}
